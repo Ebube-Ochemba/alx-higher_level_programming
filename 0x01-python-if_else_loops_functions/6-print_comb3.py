2c57c7d283e92beb928cfe11caebbd416f373d90
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-11-02 20:49:30 +0000

    task6cor

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i + 1, 10):
-        if i == 8 and j == 9:
-            print("{:d}{:d}".format(i, j))
+for tens in range(10):
+    for unit in range(tens + 1, 10):
+        if tens == 8 and unit == 9:
+            print("{:d}{:d}".format(tens, unit))
         else:
-            print("{:d}{:d}".format(i, j), end=', ')
+            print("{:d}{:d}".format(tens, unit), end=', ')
