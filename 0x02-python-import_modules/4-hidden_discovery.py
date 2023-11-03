3e5be2a5eafa9cbcf4369b9d8ff94632fd886668
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-11-03 22:35:59 +0000

    task5cor

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,7 +4,9 @@ if __name__ == "__main__":
     import importlib
 
     module = importlib.import_module('hidden_4')
-    filtered_names = [name for name in dir(module) if not name.startswith('__')]
+    filtered_names = [name
+                        for name in dir(module)
+                        if not name.startswith('__')]
     sorted_names = sorted(filtered_names)
     for name in sorted_names:
         print(name)
