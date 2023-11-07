62632384877437462f28538316714c1a76da9943
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-11-07 03:38:06 +0000

    task8cor

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    if not sentence:
-        return (0, None)
-    else:
-        return (len(sentence), sentence[0])
+
+    length = len(sentence)
+    char = sentence[0] if length > 0 else None
+
+    return (length, char)
