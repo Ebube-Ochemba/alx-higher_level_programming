55072f0d32a95e620557a5d6c0d94a30d3dae3a9
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-11-21 01:54:42 +0000

    except correction

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 
-import sys
-
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as error:
+    except (ValueError, TypeError) as error:
+        import sys
         print("Exception: {:s}".format(str(error)), file=sys.stderr)
         return False
