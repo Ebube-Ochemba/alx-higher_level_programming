7d9d4998082ab20191a363e39496d060af9c53c2
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-11-21 00:25:28 +0000

    task0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
 
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    for obj in range(0, x):
+        try:
+            print(my_list[obj], end="")
+            count += 1
+        except IndexError:
+            break
+    print("\n", end="")
+    return count
