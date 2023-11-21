cb182509185528413d5d9fc4ed14227a29299798
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-11-21 00:53:21 +0000

    task2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
 
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for obj in range(0, x):
+        try:
+            print("{:d}".format(my_list[obj]), end="")
+            count += 1
+        except (ValueError, TypeError, IndexError):
+            pass
+    print("\n", end="")
+    return count
