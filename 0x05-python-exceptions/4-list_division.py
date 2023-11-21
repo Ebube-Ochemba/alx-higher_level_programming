acf010fa6e48c06500d9b80c3bb082026022b52f
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-11-21 01:17:53 +0000

    task4

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
 
+def list_division(my_list_1, my_list_2, list_length):
+    result = [0] * list_length
+    for obj in range(list_length):
+        try:
+            result[obj] = my_list_1[obj] / my_list_2[obj]
+        except TypeError:
+            print("wrong type")
+        except ZeroDivisionError:
+            print("division by 0")
+        except IndexError:
+            print("out of range")
+        finally:
+            continue
+    return result
