5eb3b1bc6c84e075ba413e1cb4255fb780d54baf
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-11-22 17:13:30 +0000

    task2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,2 +1,31 @@
 #!/usr/bin/python3
 
+"""
+2-square
+Defines class Square with private attribute size
+"""
+
+
+class Square:
+    """
+    Defines a Square
+    ...
+
+    Attributes
+    ----------
+    size : int
+        The size of a side of the square
+    """
+    def __init__(self, size=0):
+        """
+        Parameters
+        ----------
+        size : int
+            The size of a side of the square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
