9a267bcc693be72a296bd57b5af1377dd636bd6b
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-11-22 17:33:38 +0000

    pycodestyle line41

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -38,7 +38,7 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
-    
+
     def area(self):
         """
         Calculates the area of the current square
