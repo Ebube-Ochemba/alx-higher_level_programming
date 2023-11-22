01ac8085213ff31a68df802f569a2cf01296a38f
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-11-22 17:55:50 +0000

    task4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,2 +1,79 @@
 #!/usr/bin/python3
 
+"""
+4-square
+Defines class Square with private attribute size and can calculates it area
+"""
+
+
+class Square:
+    """
+    Defines a Square
+    ...
+
+    Attributes
+    ----------
+    size : int
+        The size of a side of the square
+    """
+
+    def __init__(self, size=0):
+        """
+        Parameters
+        ----------
+        size : int
+            The size of a side of the square
+
+        Raise
+        -----
+        TypeError
+            If size is not an integer.
+        ValueError
+            If "size must be < 0.
+        """
+
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        getter
+
+        returns: The squares size.
+        """
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter
+
+        Parameters
+        ----------
+        value : int
+            The size of a side of the square
+
+        Raise
+        -----
+        TypeError
+            If size is not an integer.
+        ValueError
+            If "size must be < 0
+        """
+
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        Calculates the area of the current square
+
+        return: The area of current square.
+        """
+
+        return self.__size ** 2
