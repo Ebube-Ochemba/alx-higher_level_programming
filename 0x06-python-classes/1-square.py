1ba61dec60e151967ec9e3425d59052dc80ba242
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-11-22 16:44:39 +0000

    task1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,2 +1,26 @@
 #!/usr/bin/python3
 
+"""
+1-square
+Defines class Square with private attribute size
+"""
+
+
+class Square:
+    """
+    Defines a Square
+    ...
+
+    Attributes
+    ----------
+    size : int
+        The size of a side of the square
+    """
+    def __init__(self, size):
+        """
+        Parameters
+        ----------
+        size : int
+            The size of a side of the square
+        """
+        self._size = size
