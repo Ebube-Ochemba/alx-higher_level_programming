fc0745cdd9b041111b916842c9c974509b18ac01
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-11-24 14:18:14 +0000

    comment update

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,3 +3,11 @@
 def print_sorted_dictionary(a_dictionary):
     for key, value in sorted(a_dictionary.items()):
         print("{}: {}".format(key, value))
+
+
+"""
+The items() method returns a list of tuple pairs; the first element of each \
+        pair is a key and the second is it's value. The sorted() function \
+        sorts list of tuples based on the first element of each tuple \
+        (the key).
+"""
