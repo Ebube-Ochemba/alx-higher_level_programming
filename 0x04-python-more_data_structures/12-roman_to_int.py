92d51813ef2c9d3f9fb59083dddb288fdb2e54a4
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-11-24 15:09:25 +0000

    task12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,2 +1,30 @@
 #!/usr/bin/python3
 
+def roman_to_int(roman_string):
+    if roman_string is None or isinstance(roman_string, str) is False:
+        return 0
+
+    values = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+    }
+
+    total = 0
+    prev_value = 0
+
+    for char in roman_string:
+        curr_value = values[char]
+
+        total = total + curr_value
+
+        if curr_value > prev_value:
+            total = total - (2 * prev_value)
+
+        prev_value = curr_value
+
+    return total
