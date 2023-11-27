f2d33bc66bd624c8a7814b20e555896715fbc974
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-11-27 23:09:24 +0000

    update

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,2 +1,131 @@
 #!/usr/bin/python3
 
+"""
+3-Rectangle
+Defines class Rectangle
+"""
+
+
+class Rectangle:
+    """
+    Defines a Rectangle
+    ...
+
+    Attributes
+    ---------
+    width : int
+        The width of the rectangle.
+    height : int
+        The height of the rectangle.
+
+    methods
+    -------
+    area()
+        Calculates the area of the current Rectangle.
+    perimeter()
+        Calculates the perimeter of the current Rectangle.
+    """
+
+    def __init__(self, width=0, height=0):
+        """
+        Parameters
+        ----------
+        width : int
+            The width of the rectangle.
+        height : int
+            The height of the rectangle.
+        """
+
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """
+        getter
+
+        returns: The Rectangles width.
+        """
+
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """
+        Setter
+
+        Parameters
+        ----------
+        width : int
+            The width of a  Rectangle.
+
+        Raise
+        -----
+        TypeError
+            If width is not an integer.
+        ValueError
+            If width is < 0.
+        """
+
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """
+        getter
+
+        returns: The Rectangles height.
+        """
+
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """
+        Setter
+
+        Parameters
+        ----------
+        height : int
+            The height of a  Rectangle.
+
+        Raise
+        -----
+        TypeError
+            If height is not an integer.
+        ValueError
+            If height is < 0.
+        """
+
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """
+        Calculates the area of the current Rectangle
+
+        return: The area of current Rectangle.
+        """
+
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """
+        Calculates the perimeter of the current Rectangle
+
+        return: The perimeter of current Rectangle.
+        """
+
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return (2 * (self.__width + self.__height))
--- 0x08-python-more_classes/README.md
+++ 0x08-python-more_classes/README.md
@@ -1,29 +1,25 @@
 # Python - More Classes and Objects
 
-> This project was on more about classes and objects in Python.
+> This project was on more about OOP concepts;  classes and objects in Python.
 
 ## Summary
 
-I learnt about...
+I learnt about he special `__str__` and `__repr__` methods, their similarities and how to use them, what a class attribute is and its difference from an object attribute, what class methods and static methods are.
 
 ## Files
 
 > Each file contains the solution to a task in the project.
 
-- []():
-- []():
-- []():
-- []():
-- []():
-- []():
-- []():
-- []():
-- []():
-- []():
-- []():
-- []():
-- []():
-- []():
+- [0-rectangle.py](https://github.com/Ebube-Ochemba/alx-higher_level_programming/blob/master/0x08-python-more_classes/0-rectangle.py):
+- [1-rectangle.py](https://github.com/Ebube-Ochemba/alx-higher_level_programming/blob/master/0x08-python-more_classes/1-rectangle.py):
+- [2-rectangle.py](https://github.com/Ebube-Ochemba/alx-higher_level_programming/blob/master/0x08-python-more_classes/2-rectangle.py):
+- [3-rectangle.py](https://github.com/Ebube-Ochemba/alx-higher_level_programming/blob/master/0x08-python-more_classes/3-rectangle.py):
+- [4-rectangle.py](https://github.com/Ebube-Ochemba/alx-higher_level_programming/blob/master/0x08-python-more_classes/4-rectangle.py):
+- [5-rectangle.py](https://github.com/Ebube-Ochemba/alx-higher_level_programming/blob/master/0x08-python-more_classes/5-rectangle.py):
+- [6-rectangle.py](https://github.com/Ebube-Ochemba/alx-higher_level_programming/blob/master/0x08-python-more_classes/6-rectangle.py):
+- [7-rectangle.py](https://github.com/Ebube-Ochemba/alx-higher_level_programming/blob/master/0x08-python-more_classes/7-rectangle.py):
+- [8-rectangle.py](https://github.com/Ebube-Ochemba/alx-higher_level_programming/blob/master/0x08-python-more_classes/8-rectangle.py):
+- [9-rectangle.py](https://github.com/Ebube-Ochemba/alx-higher_level_programming/blob/master/0x08-python-more_classes/9-rectangle.py):
 - []():
 
 > - [test_files](https://github.com/Ebube-Ochemba/alx-higher_level_programming/blob/master/0x08-python-more_classes/test_files): A folder of test files. Provided by Alx.
