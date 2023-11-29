d9cc7d54a686d2af002abe6156c3853122a14e12
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-11-29 01:51:36 +0000

    0-add_integer.py, but .txt incomplete

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""
+0-add_integer
+Contains a function that add two integers.
+"""
+
+
+def add_integer(a, b=98):
+    """
+    Returns the addition of two integers. 
+
+    Parameters
+    ---------
+    a : int or float
+        The first value.
+    b : int or float
+        The second value.
+
+    Return:
+        The sum of both values.
+    """
+
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    return a + b
