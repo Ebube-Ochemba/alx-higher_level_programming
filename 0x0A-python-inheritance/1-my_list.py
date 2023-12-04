b7e66f40740ca7149ab0d4e1a0c3b5e9d15afdc8
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-12-04 23:15:17 +0000

    incomplete task1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
 
+"""1-my_list.py
+This module defines a class 'Mylist'.
+"""
+
+
+class MyList(list):
+    """Inherits from a class 'list', it is a subclass of 'list'.
+    """
+
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
+
+    def print_sorted(self):
+        """Prints the list sorted in ascending order.
+        """
+
+        print(sorted(self))
