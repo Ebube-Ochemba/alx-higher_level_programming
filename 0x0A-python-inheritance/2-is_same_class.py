930cfe562d996ccedd6aa5df56ec76298de15309
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-12-04 23:51:55 +0000

    task2

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -7,7 +7,7 @@ Defines a function is_same_class()
 
 def is_same_class(obj, a_class):
     """Checks if the object is exactly an instance of the specified class.
-    return: True if it is an excact instance, else false.
+    return: True if it is an exact instance, else false.
     """
 
     return type(obj) is a_class
