b1a737f4d596d80e3904983dd52e3ce38b66d14a
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-12-05 00:11:09 +0000

    task4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
 
+"""4-inherits_from
+Defines a function 4-inherits_from().
+"""
+
+
+def inherits_from(obj, a_class):
+    """Checks if the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class.
+
+    return: True if the object is an instance of a class that inherited
+    from the specified class, else False.
+    """
+
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
