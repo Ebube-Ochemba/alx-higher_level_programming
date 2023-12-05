d80e0472f73e6b43300a1ab6d7882b6120f72f2c
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-12-05 00:28:43 +0000

    task6

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,2 +1,28 @@
 #!/usr/bin/python3
 
+"""6-base_geometry
+Defines class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """Incomplete class
+
+    methods
+    -------
+    area()
+        An incomplete method.
+    """
+
+    def area(self):
+        """An unimplemented method
+
+        return: None
+
+        Raise
+        -----
+        Exception
+            area() is not implemented
+        """
+
+        raise Exception("area() is not implemented")
