299e4cbb23c9582ec6b50b64e958f50f7db73ba2
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-12-05 00:47:59 +0000

    incomplete

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,2 +1,46 @@
 #!/usr/bin/python3
 
+"""7-base_geometry
+Defines class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """Incomplete class
+
+    methods
+    -------
+    area()
+        An incomplete method.
+    """
+
+    def area(self):
+        """An unimplemented public attribute.
+
+        return: None.
+
+        Raise
+        -----
+        Exception
+            area() is not implemented
+        """
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Checks if 'value' is an int or is > 0.
+
+        returm: None.
+
+        Raise
+        -----
+        TypeError
+            if 'name' is not an integer.
+        ValueError
+            if value <= 0.
+        """
+
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
