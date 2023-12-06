4196c9a10d83e01cc9c7724254cc6187eabb2e72
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-12-06 03:29:26 +0000

    task6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
 
+"""
+6-load_from_json_file
+"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """creates and object from a JSON file"""
+
+    with open(filename, 'r', encoding="utf8") as file:
+        new = json.load(file)
+    return new
