4a66077aea45875cac7b2df3e742021fee2f1b8a
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-12-06 03:10:08 +0000

    task2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
 
+"""
+2-write_file
+Appends a string at the end of a text file.
+"""
+
+
+def append_write(filename="", text=""):
+    """Appends a string at the end of a text file, create if it doesn't exit.
+
+    return: The number of characters written.
+    """
+    with open(filename, 'a', encoding=("utf8")) as file:
+        written = file.write(text)
+        return written
