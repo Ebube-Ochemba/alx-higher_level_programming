f3cec516fd1ec8014f726022452be3098788c8f9
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-12-06 02:59:24 +0000

    task0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
 
+"""
+0-read_file
+Reads a text file.
+"""
+
+
+def read_file(filename=""):
+    """read a text file(utf9) and prints it to stdout"""
+
+    with open(filename, "r", encoding=("utf8")) as text:
+        lines = text.read()
+    print(lines, end='')
