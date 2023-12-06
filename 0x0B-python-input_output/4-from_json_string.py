3da63260eb473b6c50b71753431be9f02e3df528
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-12-06 03:17:00 +0000

    tasks 3 & 4

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
 
+"""
+4-to_json_string
+"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """Returns an object represented by a JSON string"""
+
+    return json.loads(my_str)
