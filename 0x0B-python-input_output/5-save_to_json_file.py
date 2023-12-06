6b8ea570bb6943240db09d74d91b12db80baa9a3
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-12-06 03:20:37 +0000

    task5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
 
+"""
+5-save_to_json_file
+"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Writes an object to a text file, using json representation"""
+
+    with open(filename, 'w', encoding="utf8") as file:
+        json.dump(my_obj, file)
