0aee41f2a835f9c45974c694014465fb781c6ae6
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-12-06 03:05:10 +0000

    task1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
 
+"""
+1-write_file
+writes a string to a text file.
+"""
+
+
+def write_file(filename="", text=""):
+    """Writes into file, create if it doesn't exit
+
+    return: The number of characters written.
+
+    """
+    
+    with open(filename, 'w', encoding=("utf8")) as file:
+        written = file.write(text)
+        return written
