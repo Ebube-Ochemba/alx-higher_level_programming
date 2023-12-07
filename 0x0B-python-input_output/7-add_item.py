bd061fe9ed3975b941cc79f84a3857badd3b036d
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-12-07 17:12:47 +0000

    task7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -14,12 +14,12 @@ load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 filename = "add_item.json"
 
 try:
-    new_list = load_from_json_file(filename)
+    arg_list = load_from_json_file(filename)
 
-except:
-    new_list = []
+except FileNotFoundError:
+    arg_list = []
 
 for arguments in argv[1:]:
-    new_list.append(arguments)
+    arg_list.append(arguments)
 
-save_to_json_file(new_list, filename)
+save_to_json_file(arg_list, filename)
