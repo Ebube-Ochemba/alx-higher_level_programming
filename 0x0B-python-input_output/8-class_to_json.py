ec1c9d22d9ca03ee24c61377e0f70dd75ac3ab18
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-12-07 18:43:09 +0000

    task8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
 
+"""
+8-class_to_json
+
+"""
+
+
+def class_to_json(obj):
+
+    """Returns dictionary description with simple data structure for JSON
+    serialization of an object.
+    """
+
+    return obj.__dict__
