4f1531f796605d61cc282cd8775fa641c0f1a085
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-12-07 22:12:50 +0000

    task12

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,2 +1,30 @@
 #!/usr/bin/python3
 
+"""
+12-pascal_triangle
+"""
+
+
+def pascal_triangle(n):
+    """Returns a list of lists of integers representing
+    the Pascal’s triangle of n.
+
+    Parameters
+    ----------
+    n : int
+        The size of the triangle.
+
+    return: An empty list if n <= 0, else a list.
+    """
+
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    for row in range(1, n):
+        new_row = [1]
+        for idx in range(1, row):
+            new_row.append(triangle[row-1][idx-1] + triangle[row-1][idx])
+        new_row.append(1)
+        triangle.append(new_row)
+    return triangle
