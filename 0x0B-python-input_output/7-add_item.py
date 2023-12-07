bfc033944dfcef91ee1c47b0387a3bb3edcdab96
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-12-07 17:06:23 +0000

    task7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,2 +1,25 @@
 #!/usr/bin/python3
 
+"""7-add_item
+This script adds all arguments to a python list and
+save them into a specific given file
+"""
+
+
+from sys import argv
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+filename = "add_item.json"
+
+try:
+    new_list = load_from_json_file(filename)
+
+except:
+    new_list = []
+
+for arguments in argv[1:]:
+    new_list.append(arguments)
+
+save_to_json_file(new_list, filename)
