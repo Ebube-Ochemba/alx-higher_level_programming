0660ed6cfbebe3415b7ca4c103ceb3738a2dfcd9
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-12-07 19:00:00 +0000

    task9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,2 +1,38 @@
 #!/usr/bin/python3
 
+"""9-student
+Defines a class student.
+"""
+
+
+class Student:
+    """
+    Defines a Student
+    ...
+
+    Attributes
+    ----------
+    first_name : string
+        undefined
+    last_name : string
+        undefined
+    age : string
+        undefined
+
+    methods
+    -------
+    to_json()
+        Returns dictionary description of an object.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Returns dictionary description with simple data structure for JSON
+        serialization of an object.
+        """
+
+        return self.__dict__
