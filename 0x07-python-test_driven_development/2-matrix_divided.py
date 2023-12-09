6e10d99ec6a8a2049fb9d0c799ef7183885f43f5
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-12-09 15:09:10 +0000

    task1 code

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -32,7 +32,6 @@ def matrix_divided(matrix, div):
     if not all(len(i) == len(matrix[0]) for i in matrix):
         raise TypeError("Each row of the matrix must have the same size")
 
-
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
