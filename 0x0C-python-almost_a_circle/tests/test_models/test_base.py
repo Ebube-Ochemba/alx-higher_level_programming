7d72be548a33f83740c33a844455fc05cfef8254
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-12-11 22:50:48 +0000

    update

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -32,12 +32,50 @@ class TestBase(unittest.TestCase):
         """Tests for the Base class docstring"""
         self.assertTrue(len(Base.__doc__) >= 1)
 
+    def setUp(self):
+        """Called for each test"""
+        Base._Base__nb_objects = 0
+
     def test_init(self):
-        """Tests that id is updated when arg is passed"""
+        """Tests that id when arg is passed"""
         base = Base(1)
         self.assertEqual(base.id, 1)
 
     def test_init_default(self):
-        """Tests that id is updated when arg isn't passed"""
+        """Tests that id when arg isn't passed"""
         base = Base()
         self.assertEqual(base.id, 1)
+
+    def test_id_nb_objects(self):
+        """Tests nb object attribute"""
+        base1 = Base()
+        base2 = Base()
+        base3 = Base()
+        self.assertEqual(base1.id, 1)
+        self.assertEqual(base2.id, 2)
+        self.assertEqual(base3.id, 3)
+
+    def test_id_mix(self):
+        """Tests nb object attr and passed id"""
+        base1 = Base()
+        base2 = Base(1024)
+        base3 = Base()
+        self.assertEqual(base1.id, 1)
+        self.assertEqual(base2.id, 1024)
+        self.assertEqual(base3.id, 2)
+
+    def test_string_id(self):
+        """Test string id"""
+        base = Base('7')
+        self.assertEqual(base.id, '7')
+
+    def test_more_args_id(self):
+        """Tests passing more args to init method"""
+        with self.assertRaises(TypeError):
+            base = Base(5, 6)
+
+    def test_access_private_attrs(self):
+        """Tests access to private attributes"""
+        base = Base()
+        with self.assertRaises(AttributeError):
+            base.__nb_objects
