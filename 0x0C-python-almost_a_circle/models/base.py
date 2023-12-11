b5ddbe5ef55d1c8ee7fab7fa9a79f8aea7bd74a4
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-12-11 10:56:58 +0000

    task1

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+base
+Defines a class Base.
+"""
+
+
+class Base:
+    """
+    Defines a class Base
+    ...
+
+    Attributes
+    ----------
+    __nb_objects (int): 
+
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        parameters
+        ----------
+        id (int): update...
+        """
+
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
