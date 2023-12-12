9a90bf629211c19504f411f71c2ae1fc05cd753e
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-12-12 03:14:16 +0000

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -158,5 +158,8 @@ class Rectangle(Base):
 
     def __str__(self):
         """Returns [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
-        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - \
-                {self.width}/{self.height}"
+        return (
+                f"[Rectangle] ({self.id}) "
+                f"{self.x}/{self.y} - "
+                f"{self.width}/{self.height}"
+            )
