1b2171fe2adbe84a7a68f590e4f02f5615af5935
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-12-13 03:17:32 +0000

    task12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -15,6 +15,8 @@ class Square(Rectangle):
     -------
     __str__()
         return [Square] (<id>) <x>/<y> - <size>
+    update()
+        Updates the square's attributes.
     """
     def __init__(self, size, x=0, y=0, id=None):
         """
@@ -45,3 +47,21 @@ class Square(Rectangle):
     def __str__(self):
         """return [Square] (<id>) <x>/<y> - <size>"""
         return f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}"
+
+    def update(self, *args, **kwargs):
+        """Update the square attributes"""
+        if args is not None and len(args) != 0:
+            list_atr = ['id', 'size', 'x', 'y']
+            for i in range(len(args)):
+                if list_atr[i] == 'size':
+                    setattr(self, 'width', args[i])
+                    setattr(self, 'height', args[i])
+                else:
+                    setattr(self, list_atr[i], args[i])
+        else:
+            for key, value in kwargs.items():
+                if key == 'size':
+                    setattr(self, 'width', value)
+                    setattr(self, 'height', value)
+                else:
+                    setattr(self, key, value)
