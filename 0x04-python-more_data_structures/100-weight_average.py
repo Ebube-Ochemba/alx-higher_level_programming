6477be89e044f25d2d50a6b9310916a0508ee557
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2023-12-28 14:54:51 +0000

    task13 comment

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-# = ((1 * 2) + (2 * 1) + (3 * 10) + (4 * 2)) / (2 + 1 + 10 + 2)
+# Grades(score, weight) = [(A, 2), (B, 1), (C, 10), (D, 2)]
+# Weighted average = ((A * 2) + (B * 1) + (C* 10) + (D * 2)) / (2 + 1 + 10 + 2)
 
 
 def weight_average(my_list=[]):
