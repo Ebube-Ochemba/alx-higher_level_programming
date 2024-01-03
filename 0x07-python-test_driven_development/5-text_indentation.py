e30b954f80e5fbcb0ad3f512669f1a2902ee7e9e
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2024-01-03 00:05:51 +0000

    updated

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -13,8 +13,8 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    for char in text:
-        if char in [".", "?", ":"]:
-            print(char, end='\n\n')
-        else:
-            print(char, end='')
+    for char in ".:?":
+        text = (char + "\n\n").join(
+            [line.strip(" ") for line in text.split(char)])
+
+    print(f"{text}", end="")
