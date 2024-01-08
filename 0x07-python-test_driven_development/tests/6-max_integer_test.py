f72608dae63d6ffc857162419a1a86e16fe77f26
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2024-01-08 01:05:35 +0000

    pycodestyle

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """Unittest for max_integer([..])
 """
+
 import unittest
 max_integer = __import__('6-max_integer').max_integer
+
 
 class TestMaxInteger(unittest.TestCase):
     """A class to test max_integer"""
@@ -15,6 +17,7 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_negative_numbers(self):
         self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+
 
 if __name__ == '__main__':
     unittest.main()
