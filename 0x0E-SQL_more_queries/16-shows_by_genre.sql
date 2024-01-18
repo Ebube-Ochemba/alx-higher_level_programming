75a05067705662ddf3411469a784ab454eb76aa6
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2024-01-18 12:25:58 +0000

    task16 correction

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,9 +1,17 @@
--- List all shows and genres linked to show from 'hbtn_0d_tvshows'
--- If show doesn't have a genre, display NULL in genre column
--- Each record should display tv_shows.title, tv_genres.name
--- Results must be sorted in ascending order by show title
+-- Import the database dump from hbtn_0d_tvshows to your MySQL server: download (same as 15-comedy_only.sql)
+
+-- Write a script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
+
+-- If a show doesn’t have a genre, display NULL in the genre column
+-- Each record should display: tv_shows.title - tv_genres.name
+-- Results must be sorted in ascending order by the show title and genre name
+-- You can use only one SELECT statement
+-- The database name will be passed as an argument of the mysql command
+
 SELECT tv_shows.title, tv_genres.name
-FROM tv_shows ts
-LEFT JOIN tv_show_genres tsg ON ts.id = tsg.show_id
-LEFT JOIN tv_genres tg ON tsg.genre_id = tg.id
-ORDER BY ts.title, tg.name ASC;
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title, tv_genres.name ASC;
