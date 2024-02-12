9f68b3c56047c6cd416a829c5da92054f0b4ede1
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2024-02-12 17:06:03 +0000

    task1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// A script that prints 3 lines
+
+const myVar = 'C is fun';
+console.log(myVar);
+const myVar1 = 'Python is cool';
+console.log(myVar1);
+const myVar2 = 'JavaScript is amazing';
+console.log(myVar2);
