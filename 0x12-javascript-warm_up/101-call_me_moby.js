498b5e25bb583c94f66c3c476a3067915a18b08b
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2024-02-13 18:59:40 +0000

    task0

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// a\A function that executes x times a function.
+// A function that executes x times a function.
 
 exports.callMeMoby = function (x, theFunction) {
   for (let i = 0; i < x; i++) {
