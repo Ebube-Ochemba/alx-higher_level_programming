4e467a216fd41242b09af18f6b704840793456ac
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2024-02-13 03:01:11 +0000

    correction

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-// A function that increments and calls a function. 
+// A function that increments and calls a function.
 
-exports.addMeMaybe = function(number, theFunction) {
+exports.addMeMaybe = function (number, theFunction) {
   theFunction(number + 1);
 };
