1f31bf01d2177c5e5e6af1b646997e436cef0ac4
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2024-02-14 04:49:59 +0000

    task4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,2 +1,34 @@
 #!/usr/bin/node
+// A class Rectangle that defines a rectangle.
 
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let idx = 0; idx < this.height; idx++) {
+      let row = '';
+      for (let idy = 0; idy < this.width; idy++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+
+  rotate () {
+    const tmp = this.height;
+    this.height = this.width;
+    this.width = tmp;
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+}
+
+module.exports = Rectangle;
