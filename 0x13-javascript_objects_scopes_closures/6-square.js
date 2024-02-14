0e153b9bb0426861cfe5247cd3a7e749254d2f41
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2024-02-14 05:10:27 +0000

    task6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,2 +1,18 @@
 #!/usr/bin/node
+// A class Square that defines a square and inherits from Square.
 
+const Square5 = require('./5-square');
+
+class Square extends Square5 {
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let idx = 0; idx < this.height; idx++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+
+module.exports = Square;
