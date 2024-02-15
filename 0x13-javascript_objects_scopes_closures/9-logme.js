e23f8ad19fd1740a092ae27d00e66e862462b333
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2024-02-15 03:51:30 +0000

    task9

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,2 +1,8 @@
 #!/usr/bin/node
+// A function that prints the number of arguments already printed and the new argument value.
 
+let ClosureCheck = 0;
+exports.logMe = function (item) {
+  console.log(ClosureCheck + ': ' + item);
+  ClosureCheck++;
+};
