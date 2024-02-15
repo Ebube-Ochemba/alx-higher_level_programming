517e9091f2292f819433ad453955bb3e470ffb07
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2024-02-15 03:45:26 +0000

    task8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,2 +1,11 @@
 #!/usr/bin/node
+// A function that returns the reversed version of a list.
 
+exports.esrever = function (list) {
+  const revList = [];
+
+  for (const idx in list) {
+    revList.unshift(list[idx]);
+  }
+  return (revList);
+};
