3d9c1b1bfc2ba268ba99709b76dc64455970061d
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2024-02-15 04:19:17 +0000

    task10

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,2 +1,9 @@
 #!/usr/bin/node
+// A function that converts a number from base 10 to another base passed as argument.
 
+exports.converter = function (base) {
+  function convert (num) {
+    return (num.toString(base));
+  }
+  return (convert);
+};
