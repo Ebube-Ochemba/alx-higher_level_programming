59b1667d5b6e3d04b9d2a40eab584f06d99c2083
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2024-02-19 13:56:02 +0000

    task8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+A script that prints the first State object from the database hbtn_0e_6_usa
+"""
+
+import sqlalchemy
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+
+    usrnm, psswd, dtbs = sys.argv[1:]
+    link = "mysql+mysqldb://{}:{}@localhost/{}".format(usrnm, psswd, dtbs)
+    engine = create_engine(link)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    first_state_query = session.query(State).order_by(State.id).first()
+    if first_state_query is not None:
+        print("{}: {}".format(first_state_query.id, first_state_query.name))
+    else:
+        print("Nothing")
+
+    session.close()
