c576a0321e4f229b3e21174e2ac9fb7ba5dac9ac
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2024-02-19 04:34:00 +0000

    task7

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+A script that lists all State objects from the database hbtn_0e_6_usa.
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+
+    usrnm, psswd, dtbs = sys.argv[1:]
+    link = "mysql+mysqldb://{}:{}@localhost/{}".format(usrnm, psswd, dtbs)
+    engine = create_engine(link)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states_query = session.query(State).order_by(State.id.asc())
+    for state in states_query.all():
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
