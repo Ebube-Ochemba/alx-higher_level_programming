b13e332ba32739cf04d2bb29b648dd59d0a8226e
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2024-02-19 14:30:04 +0000

    task11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+A script that adds the State object “Louisiana” to the database hbtn_0e_6_usa.
+"""
+
+import sqlalchemy
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+
+    usrnm, psswd, dtbs = sys.argv[1:]
+    link = "mysql+mysqldb://{}:{}@localhost/{}".format(usrnm, psswd, dtbs)
+    engine = create_engine(link)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name='Louisiana')
+    session.add(new_state)
+
+    state = session.query(State).filter_by(name='Louisiana').first()
+    print(str(state.id))
+
+    session.commit()
+    session.close()
