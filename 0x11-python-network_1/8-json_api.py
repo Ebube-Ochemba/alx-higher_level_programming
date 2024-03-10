bfcc68414b3ecf123f55c79e97631bc6a566a92e
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2024-03-10 19:07:58 +0000

    task8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""A Python script that takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    letter = argv[1] if len(argv) > 1 else ''
+
+    try:
+        response = requests.post('http://0.0.0.0:5000/search_user', data={'q': letter})
+        json = response.json()
+        if json:
+            print('[{}] {}'.format(json['id'], json['name']))
+        else:
+            print('No result')
+    except ValueError:
+        print('Not a valid JSON')
