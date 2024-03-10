1e1045eb4696e04a14f1d08488cb7012d859f415
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2024-03-10 19:10:37 +0000

    pycodestyle

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -10,7 +10,8 @@ if __name__ == "__main__":
     letter = argv[1] if len(argv) > 1 else ''
 
     try:
-        response = requests.post('http://0.0.0.0:5000/search_user', data={'q': letter})
+        response = requests.post('http://0.0.0.0:5000/search_user',
+                                 data={'q': letter})
         json = response.json()
         if json:
             print('[{}] {}'.format(json['id'], json['name']))
