6e251a89d7e572c804b173ce680abb5c3c9bf0ab
Ebube-Ochemba <123279755+Ebube-Ochemba@users.noreply.github.com>
2024-03-10 18:25:17 +0000

    final correction

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"""A Python script that fetches https://alx-intranet.hbtn.io/status"""
-
-import requests
+"""A Python script that fetches https://alx-intranet.hbtn.io/status."""
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    import requests
 
-    response = requests.get("https://intranet.hbtn.io/status")
+    response = requests.get('https://alx-intranet.hbtn.io/status')
     res = response.text
     print("Body response:")
-    print("\t- type: {}".format(type(res)))
-    print("\t- content: {}".format(res))
+    print(f"\t- type: {type(res)}")
+    print(f"\t- content: {res}")
