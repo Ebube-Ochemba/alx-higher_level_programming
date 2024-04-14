5e0343288dc0d5e494d46dc25bdfc2b62e41d9b6
Ebube-Ochemba <ebube17@gmail.com>
2024-04-14 03:14:01 +0100

    task17

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Script that creates the State "California" with the City "San Francisco"
+from the database hbtn_0e_100_usa.
+"""
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
+from relationship_city import City
+
+if __name__ == "__main__":
+
+    usrnm, psswd, dtbs = argv[1:]
+    link = "mysql+mysqldb://{}:{}@localhost/{}".format(usrnm, psswd, dtbs)
+    engine = create_engine(link)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    cities = session.query(City).all()
+    for city in cities:
+        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
+
+    session.close()
