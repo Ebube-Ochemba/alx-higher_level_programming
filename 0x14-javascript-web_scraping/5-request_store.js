31a9db57d15210869c4761d5fd14fc9afc18b137
Ebube-Ochemba <ebube17@gmail.com>
2024-05-21 18:01:19 +0100

    pre debug

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+// Import the request module
+const request = require('request');
+
+// Import the file system module
+const fs = require('fs');
+
+// Get the API URL from the command line arguments
+const url = process.argv[2];
+
+// Get the path to store the body response from the command line arguments
+const filePath = process.argv[3];
+
+// Use the get method of the request module to send a GET request to the API URL
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  if (response.statusCode !== 200) {
+    console.error(`Error: ${response.statusCode}`);
+    return;
+  }
+
+  fs.writeFile(filePath, body, 'utf8', (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(`The file has been saved as ${filePath}`);
+    }
+  });
+});
