d407662f529551072fc9f62b8d88c67e86c5cd8d
Ebube-Ochemba <ebube17@gmail.com>
2024-05-21 16:04:50 +0100

    semistandard

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+// Import the file system module
+const fs = require('fs');
+
+// Get the file path from the command line arguments
+const filePath = process.argv[2];
+
+// Use the readFile function to read the file asynchronously
+fs.readFile(filePath, 'utf-8', function (err, data) {
+  // If an error occurred during reading, print the error
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
