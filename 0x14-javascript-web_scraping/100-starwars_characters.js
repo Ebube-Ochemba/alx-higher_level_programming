174bcc96757fcd76b20f5a8cd35a5e0f43e4c0a1
Ebube-Ochemba <ebube17@gmail.com>
2024-05-21 22:25:14 +0100

    semistandard

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -11,31 +11,31 @@ const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
 // Use the get method of the request module to send a GET request to the API URL
 request.get(apiUrl, (error, response, body) => {
-    if (error) {
-      console.error(error);
-      return;
-    }
-    if (response.statusCode !== 200) {
-      console.error(`Error: ${response.statusCode}`);
-      return;
-    }
+  if (error) {
+    console.error(error);
+    return;
+  }
+  if (response.statusCode !== 200) {
+    console.error(`Error: ${response.statusCode}`);
+    return;
+  }
 
-    const film = JSON.parse(body);
-    const characters = film.characters;
+  const film = JSON.parse(body);
+  const characters = film.characters;
 
-    characters.forEach(characterUrl => {
-        request.get(characterUrl, (error, response, body) => {
-            if (error) {
-                console.error(error);
-                return;
-            }
-            if (response.statusCode !== 200) {
-            console.error(`Error: ${response.statusCode}`);
-            return;
-            }
+  characters.forEach(characterUrl => {
+    request.get(characterUrl, (error, response, body) => {
+      if (error) {
+        console.error(error);
+        return;
+      }
+      if (response.statusCode !== 200) {
+        console.error(`Error: ${response.statusCode}`);
+        return;
+      }
 
-            const character = JSON.parse(body);
-            console.log(character.name);
-        });
+      const character = JSON.parse(body);
+      console.log(character.name);
     });
+  });
 });
