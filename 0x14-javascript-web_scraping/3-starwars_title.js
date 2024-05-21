b9be8ae45c61744fa177585484366d1e3accef6a
Ebube-Ochemba <ebube17@gmail.com>
2024-05-21 16:58:10 +0100

    done

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+// Import the request module
+const request = require('request');
+
+// Get the movie ID from the command line arguments
+const movieId = process.argv[2];
+
+// Construct the API URL using the movie ID
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+// Use the get method of the request module to send a GET request to the API URL
+request.get(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else if (response.statusCode !== 200) {
+    console.error(`Error: ${response.statusCode}`);
+  } else {
+    const movie = JSON.parse(body);
+    console.log(movie.title);
+  }
+});
