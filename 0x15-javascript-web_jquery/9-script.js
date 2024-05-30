10ee291304b70b15f1ecedc951109e7520f2a6d6
Ebube-Ochemba <ebube17@gmail.com>
2024-05-30 01:53:27 +0100

    done

---
+++ 0x15-javascript-web_jquery/9-script.js
@@ -0,0 +1,15 @@
+/* global $ */
+$(document).ready(function () {
+    $.ajax({
+      url: 'https://hellosalut.stefanbohacek.dev/?lang=fr',
+      type: 'GET',
+      dataType: 'json',
+      success: function (data) {
+        $('#hello').text(data.hello);
+      },
+      error: function (xhr, status, error) {
+        console.error('Error fetching translation:', error);
+      }
+    });
+  });
+  
