8803a27c94991f57788469154460c566c2ba8815
Ebube-Ochemba <ebube17@gmail.com>
2024-05-30 01:42:14 +0100

    done

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,15 @@
+/* global $ */
+$(document).ready(function () {
+    $.ajax({
+      url: 'https://swapi-api.alx-tools.com/api/people/5/?format=json',
+      type: 'GET',
+      dataType: 'json',
+      success: function (data) {
+        $('#character').text(data.name);
+      },
+      error: function (xhr, status, error) {
+        console.error('Error fetching character:', error);
+      }
+    });
+  });
+  
