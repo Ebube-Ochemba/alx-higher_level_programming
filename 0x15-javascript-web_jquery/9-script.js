73b23485a8abc8a514597a4ab94d2c54ace342cf
Ebube-Ochemba <ebube17@gmail.com>
2024-05-30 01:53:44 +0100

    semistandard

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,15 +1,14 @@
 /* global $ */
 $(document).ready(function () {
-    $.ajax({
-      url: 'https://hellosalut.stefanbohacek.dev/?lang=fr',
-      type: 'GET',
-      dataType: 'json',
-      success: function (data) {
-        $('#hello').text(data.hello);
-      },
-      error: function (xhr, status, error) {
-        console.error('Error fetching translation:', error);
-      }
-    });
+  $.ajax({
+    url: 'https://hellosalut.stefanbohacek.dev/?lang=fr',
+    type: 'GET',
+    dataType: 'json',
+    success: function (data) {
+      $('#hello').text(data.hello);
+    },
+    error: function (xhr, status, error) {
+      console.error('Error fetching translation:', error);
+    }
   });
-  
+});
